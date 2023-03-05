2989def82b93e0906ca5de6b4ef3b689687b8fa0
Stefan Haller <stefan@haller-berlin.de>
2023-03-05 14:15:31 +0100

    Add command to open git difftool

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -46,6 +46,7 @@ type TranslationSet struct {
 	ToggleStaged                        string
 	ToggleStagedAll                     string
 	ToggleTreeView                      string
+	OpenDiffTool                        string
 	OpenMergeTool                       string
 	Refresh                             string
 	Push                                string
@@ -776,6 +777,7 @@ type Actions struct {
 	Undo                              string
 	Redo                              string
 	CopyPullRequestURL                string
+	OpenDiffTool                      string
 	OpenMergeTool                     string
 	OpenCommitInBrowser               string
 	OpenPullRequest                   string
@@ -856,6 +858,7 @@ func EnglishTranslationSet() TranslationSet {
 		ToggleStaged:                        "Toggle staged",
 		ToggleStagedAll:                     "Stage/unstage all",
 		ToggleTreeView:                      "Toggle file tree view",
+		OpenDiffTool:                        "Open external diff tool (git difftool)",
 		OpenMergeTool:                       "Open external merge tool (git mergetool)",
 		Refresh:                             "Refresh",
 		Push:                                "Push",
@@ -1546,6 +1549,7 @@ func EnglishTranslationSet() TranslationSet {
 			Undo:                              "Undo",
 			Redo:                              "Redo",
 			CopyPullRequestURL:                "Copy pull request URL",
+			OpenDiffTool:                      "Open diff tool",
 			OpenMergeTool:                     "Open merge tool",
 			OpenCommitInBrowser:               "Open commit in browser",
 			OpenPullRequest:                   "Open pull request in browser",
