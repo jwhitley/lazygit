c950cc6e210996105b742e55e14e104176641fc5
John Whitley <john@luminous-studios.com>
2024-01-03 12:04:06 -0800

    Emit the failed command to error log on failure
    When an unexpected command failure occurs, also print the failed
    command to the error log.

--- pkg/tasks/tasks.go
+++ pkg/tasks/tasks.go
@@ -272,6 +272,7 @@ func (self *ViewBufferManager) NewCmdTask(start func() (*exec.Cmd, io.Reader), p
 				// it's fine if we've killed this program ourselves
 				if !strings.Contains(err.Error(), "signal: killed") {
 					self.Log.Errorf("Unexpected error when running cmd task: %v", err)
+					self.Log.Errorf("Failed command: %v %v", cmd.Path, cmd.Args)
 				}
 			}
 
